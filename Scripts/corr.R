19ab3f6aaa4c9bdcebfa9d243ec1bb0eb704b6ee
Ubiratan da Motta Filho <ubfilho16@gmail.com>
2021-08-22 15:34:44 -0300

    Add docstring in corr.R

--- Scripts/corr.R
+++ Scripts/corr.R
@@ -1,6 +1,10 @@
 library(tidyverse)
 
 corr <- function(directory, threshold=0){
+        # returns a vector of a correlation of nitrate and sulfate columns
+        # in each csv file, selecting only the files that has more complete
+        # cases than the given `threshold`
+        
         list_files <- list.files(directory)
         
         df_complete <- complete(directory)
